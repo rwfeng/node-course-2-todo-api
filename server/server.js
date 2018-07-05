80becfe700fc329fda254dd6edd045d81eac8faa
rwfeng <rwfeng@ucsd.edu>
2018-07-05 00:04:00 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-
 var app = express();
 
 app.use(bodyParser.json());
@@ -14,11 +13,20 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000, () => {
