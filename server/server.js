64cda9491badfeb81d2501f9be1a34209e8b354f
rwfeng <rwfeng@ucsd.edu>
2018-07-05 11:31:41 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const{ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -29,21 +30,21 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.get('/todos/:id', (req, res)=>{
+app.get('/todos/:id', (req, res) => {
+    // res.send('hello there');
     let id = req.params.id;
     if(!ObjectID.isValid(id)){
         return res.status(404).send('not a valid id');
-    }
-    Todo.findById(id).then((todo)=>{
-       if(todo){
-           return res.send({todo});
-       }
+      }
+      Todo.findById(id).then((todo)=>{
+         if(todo){
+             return res.send({todo});
+         }
 
-       res.status(404).send('could not find id');
-    }).catch((e)=>{
-        res.status(400).send();
-    });
-
+         res.status(404).send('could not find id');
+      }).catch((e)=>{
+          res.status(400).send();
+      });
 });
 
 app.listen(3000, () => {
